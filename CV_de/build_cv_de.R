b59157a5cd67f26035f412b9b45067b7bcb4a2bc
Moritz Rösch <moritz.roesch@gmx.de>
2022-04-04 08:28:45 +0200

    build cv files de

---
+++ CV_de/build_cv_de.R
@@ -0,0 +1,29 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+##
+## Script name: cv_de.R
+##
+## Purpose of script: Build datadriven cv (german version)
+##
+## Author: Moritz R?sch
+##
+## Date: 2022-04-04
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
+
+# Packages ----------------------------------------------------------------
+
+#remotes::install_github('mitchelloharawild/icons@v0.1.0') # needed for icon package dependency
+#devtools::install_github("nstrayer/datadrivencv")
+library(datadrivencv)
+
+# Set up CV files ---------------------------------------------------------
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Moritz Rösch",
+  data_location = "https://docs.google.com/spreadsheets/d/17-iIigcHOk23r3Ki566MthMHw0o5GXdRyXkeIrvjbz0/edit#gid=917338460",
+  #pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+  #html_location = "nickstrayer.me/cv/",
+  source_location = "https://github.com/moritzroesch/automated-cv"
+)
